e841b4931d19838645f1558918105ed962c4fb79
mkubasz <mkubasz@gmail.com>
2019-03-07 23:05:30 +0100

    Add pages

---
+++ lib/pages/product.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  
+  ProductPage(this.title, this.imageUrl);
+  
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+    } ,child: Scaffold(
+      appBar: AppBar(
+        title: Text('Product Detail'),
+      ),
+      // body: Center(
+      //   child: Text('On the Product Page'),
+      // ),
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(padding: EdgeInsets.all(10.0), child: Text(title)),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('DELETE'),
+                  onPressed: () => Navigator.pop(context, true),
+                ))
+          ],
+        ),
+      ),
+    ));
+  }
+}
